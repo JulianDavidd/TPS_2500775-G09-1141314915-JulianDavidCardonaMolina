a53306be76ea9ff7ca3b32b5115a9cdb958c035f
JulianDavidd <jc4073679@gmail.com>
2022-10-11 15:28:31 -0500

    innerHTML

--- html_snippets/juego mokepon/js/mokepon.js
+++ html_snippets/juego mokepon/js/mokepon.js
@@ -1,8 +1,28 @@
+function iniciarJuego(){
+    let botonMascotaJugador=document.getElementById('boton_mascota')
+botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
 
+}
 
 
 function seleccionarMascotaJugador(){
-    alert('SELECCIONASTE TU MASCOTA')}
+    let inputHipodoge = document.getElementById("hipodoge")
+    let inputCapipepo = document.getElementById("Capipepo")
+    let inputSol = document.getElementById("sol")
+    let spanMascotaJugador = document.getElementById("mascota-jugador")
 
-let botonMascotaJugador=document.getElementById('boton_mascota')
-botonMascotaJugador.addEventListener('click',seleccionarMascotaJugador)
+
+
+
+    if (inputHipodoge.checked){
+       {spanMascotaJugador.innerHTML = "hipodoge"} 
+    } else if (inputCapipepo.checked) {
+        {spanMascotaJugador.innerHTML = "Capipepo"}
+    }else if (inputSol.checked){
+        {spanMascotaJugador.innerHTML = "sol"}
+    }else {
+        alert("seleciona una mascota")
+    }
+}
+
+window.addEventListener("load", iniciarJuego)
