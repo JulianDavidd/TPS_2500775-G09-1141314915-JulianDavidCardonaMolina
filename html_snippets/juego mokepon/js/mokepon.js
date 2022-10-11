60cafbcb450e84742ae25ae7056d6bda0a08828e
JulianDavidd <jc4073679@gmail.com>
2022-10-11 15:49:37 -0500

    MascotaEnemigo

--- html_snippets/juego mokepon/js/mokepon.js
+++ html_snippets/juego mokepon/js/mokepon.js
@@ -23,6 +23,29 @@ function seleccionarMascotaJugador(){
     }else {
         alert("seleciona una mascota")
     }
+
+    seleccionarMascotaEnemigo()
+}
+
+
+
+function seleccionarMascotaEnemigo() {
+    let ataqueAleatorio  = aleatorio(1,3)
+    let spanMascotaEnemigo = document.getElementById("mascota-enemigo")
+
+
+    if (ataqueAleatorio == 1) {
+        spanMascotaEnemigo.innerHTML = "hipodoge"
+    }else if (ataqueAleatorio == 2) {
+        spanMascotaEnemigo.innerHTML = "capipepo"
+    }else {
+        spanMascotaEnemigo.innerHTML =  "sol"
+    }
+}
+
+
+function aleatorio(min,max) {
+    return Math.floor(Math.random() *  (max - min + 1) +min)
 }
 
 window.addEventListener("load", iniciarJuego)
